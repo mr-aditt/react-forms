7584653552279bd896b2fdbeb44e892d9217e14f
mr-aditt <mr.aditt@outlook.com>
2021-12-25 18:14:23 +0530

    Updates

---
+++ precache-manifest.94f1a4f25741b9772f265d733304ff5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb2de9077e2fa2a723d7",
+    "url": "/react-forms/static/css/main.05a2f140.chunk.css"
+  },
+  {
+    "revision": "fb2de9077e2fa2a723d7",
+    "url": "/react-forms/static/js/main.fb2de907.chunk.js"
+  },
+  {
+    "revision": "b36e433caf61664e0962",
+    "url": "/react-forms/static/js/1.b36e433c.chunk.js"
+  },
+  {
+    "revision": "844a22b63b56059d3b11",
+    "url": "/react-forms/static/js/runtime~main.844a22b6.js"
+  },
+  {
+    "revision": "4897c553cfae350d346594b9b96d194e",
+    "url": "/react-forms/index.html"
+  }
+];
